48a376dd5500707d935ed7d18ff6e8d8a0b88485
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-05-17 15:16:02 +1000

    add scrollview

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 
+// text written as short paragraphs 
+// then use \n to identify break in text (split) to show small amount of text,
+// then tap screen to show next block of text (and move old text up)
+// until it finishes, then show buttons
+// and have ability to scroll up through text
+// click button
+// buttons fade out 
+// line of italic or something text shows choice made
+// new text appears (same as when been tapping before)
+
 const TextArea = () => {
   return (
     <View style={styles.textArea}>
       <Text style={styles.text}>
-        The air is warm. A light breeze blows the dirt off the road before you
-        into small clouds, before settling back down again.
+        {`The air is warm. A light breeze blows the dirt off the road before you
+        into small clouds, before settling back down again.\n`}
       </Text>
     </View>
   );
--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import Button from '../../components/Button';
 import TextArea from '../../components/TextArea';
 
 const MainScreen = () => {
   return (
-    <View style={styles.container}>
-      <TextArea />
-      <View style={styles.buttonContainer}>
-        <View style={{ flexDirection: 'row' }}>
-          <Button choice={'Button 1'} />
-          <View style={styles.padding} />
-          <Button choice={'Button 2'} />
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.container}>
+        <TextArea />
+        <View style={styles.buttonContainer}>
+          <View style={{ flexDirection: 'row' }}>
+            <Button choice={'Button 1'} />
+            <View style={styles.padding} />
+            <Button choice={'Button 2'} />
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
